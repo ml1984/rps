92ffee0cb5d1971d3702f2f0cd476dd473beb2da
ml1984 <michaelandrewluckey@gmail.com>
2016-02-24 18:48:37 -0800

    added timer

--- app/assets/javascripts/controllers/app_ctrl.js
+++ app/assets/javascripts/controllers/app_ctrl.js
@@ -1,10 +1,30 @@
-angular.module('AppController', ['GameService']).controller('AppCtrl', ['$scope', 'Game',
-  function($scope, Game) {
+angular.module('AppController', ['GameService']).controller('AppCtrl', ['$scope', 'Game', '$interval', '$document',
+  function($scope, Game, $interval, $document) {
     new Game({}).create().then(function(results){
       $scope.game = results;
     });
+    $scope.counter = 3;
+    $scope.shouldListen = false;
+    $scope.keypresses = [];
+
+    document.body.addEventListener('keydown', function(e) {
+      if ($scope.shouldListen) {
+        $scope.keypresses.push(e.code);
+      }
+      if ($scope.keypresses.length >=2) {
+        $scope.shouldListen= false;
+      }
+    })
 
     $scope.saveGame = function () {
       $scope.game.save();
+      var counter = $interval(function() {
+        $scope.counter--;
+        if ($scope.counter === 0) {
+          $interval.cancel(counter)
+          $scope.shouldListen = true;
+        }
+      }, 1000)
+
     }
   }]);
